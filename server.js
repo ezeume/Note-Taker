f92ef7583f0954ab0d1be8a2146b02e41cfc2721
Nelson Ezeume <nelsonezeume@yahoo.com>
2020-03-08 04:39:25 -0400

    commit send file to html

--- server.js
+++ server.js
@@ -1,24 +1,60 @@
 // Dependencies
 // =============================================================
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
+
+//Output path
+
+
+//Get routes
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("_dirname"));
+var notes = require("./db/db.json");
+
+// API ROUTES
+app.get("/api/notes", function (req, res) {
+    res.json(notes);
+});
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "puplic/notes.html"));
+  });
+
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "puplic/index.html"));
+  });
+
+
+// Sets up the Express app to handle data parsing
+
+
+//post
 
 
 
 
-// var note = [{
-//     noteTitle: "",
-//     noteText: ""
-// }];
+
+
+
+//writeFile
+
+fs.writeFile(notes.html, html, function (err) {
+
+    if (err) {
+        return console.log(err);
+    }
+
+
+});
 
 
 
@@ -35,13 +71,13 @@ app.get("/note", function (req, res) {
 
 
 
-// Displays all characters
-app.get("/api/characters", function (req, res) {
-    return res.json(characters);
-});
-// when the user goes to localhost:3000/notes the user will see the notes.html file
+// // Displays all characters
+// app.get("/api/characters", function (req, res) {
+//     return res.json(characters);
+// });
+// // when the user goes to localhost:3000/notes the user will see the notes.html file
 
-// when the user goes to localhost:3000 the user will see the index.html file
+// // when the user goes to localhost:3000 the user will see the index.html file
 
 
 
